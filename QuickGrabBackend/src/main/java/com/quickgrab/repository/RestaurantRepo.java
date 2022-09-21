cb7f519a766fa6c04c22e98a2a2553174acf5bcd
SakshiDhote22 <110605988+SakshiDhote22@users.noreply.github.com>
2022-09-21 10:14:04 +0530

    Add files via upload

---
+++ QuickGrabBackend/src/main/java/com/quickgrab/repository/RestaurantRepo.java
@@ -0,0 +1,16 @@
+package com.quickgrab.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.quickgrab.model.CustomerModel;
+import com.quickgrab.model.RestaurantModel;
+
+@Repository
+public interface RestaurantRepo extends JpaRepository<RestaurantModel, Integer> {
+
+RestaurantModel findByRestContact(String restContact);
+	
+RestaurantModel findByRestName(String restName);
+	
+}
